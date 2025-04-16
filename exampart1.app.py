b02410e3d4e1e5be93f5f3e97310379729f32ed7
Md-Arshad-Khan <138754973+Md-Arshad-Khan@users.noreply.github.com>
2025-04-16 17:43:09 -0500

    Update exampart1.app.py

--- exampart1.app.py
+++ exampart1.app.py
@@ -1,3 +1,23 @@
 import pandas as pd
 import matplotlib.pylab as plt
 filename="https://raw.githubusercontent.com/klamsal/Fall2024Exam/refs/heads/main/auto.csv"
+headers = ["symboling","normalized-losses","make","fuel-type","aspiration", "num-of-doors","body-style",
+         "drive-wheels","engine-location","wheel-base", "length","width","height","curb-weight","engine-type",
+         "num-of-cylinders", "engine-size","fuel-system","bore","stroke","compression-ratio","horsepower",
+         "peak-rpm","city-mpg","highway-mpg","price"]
+df = pd.read_csv(filename, names = headers)
+df.head()
+import numpy as np
+
+# replace "?" to NaN
+df.replace("?", np.nan, inplace = True)
+df.head(5)
+missing_data = df.isnull()
+missing_data.head(5)
+for column in missing_data.columns.values.tolist():
+    print(column)
+    print (missing_data[column].value_counts())
+    print("")
+  avg_norm_loss = df["normalized-losses"].astype("float").mean(axis=0)
+print("Average of normalized-losses:", avg_norm_loss)
+df["normalized-losses"].replace(np.nan, avg_norm_loss, inplace=True)
